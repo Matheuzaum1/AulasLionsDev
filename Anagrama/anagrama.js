2e638649de6e8de6bc7d37417b3dc2b11b39e3d6
Matheuzaum1 <matheushenriquerosendo@gmail.com>
2024-06-25 08:39:39 -0300

    update anagrama.js

--- Anagrama/anagrama.js
+++ Anagrama/anagrama.js
@@ -1,23 +1,23 @@
 let palavra;
-let anagrama = []; // Inicializa anagrama como um array vazio
+let anagramas = [];
 
-console.log("Digite uma palavra para gerar um anagrama");
+console.log("Digite uma palavra para fazermos os anagramas dela.");
 
-process.stdin.on("data", function(data) {
-    palavra = data.toString().trim();
-    
-    palavra = palavra.split("");
-    for (let i = 0; i < palavra.length; i++) {
-        for (let j = 0; j < palavra.length; j++) {
-            let aux = palavra[i];
-            palavra[i] = palavra[j];
-            palavra[j] = aux;
+process.stdin.on("data", function (data) {
+  palavra = data.toString().trim();
 
-            let novoAnagrama = palavra.join("");
-            if (!anagrama.includes(novoAnagrama)) {
-                console.log(novoAnagrama);
-                anagrama.push(novoAnagrama); // Adiciona novoAnagrama ao array anagrama
-            }
-        }
+  palavra = palavra.split("");
+
+  for (let i = 0; i < palavra.length; i++) {
+    for (let j = 0; j < palavra.length; j++) {
+      let aux = palavra[i];
+      palavra[i] = palavra[j];
+      palavra[j] = aux;
+
+      if (!anagramas.includes(palavra.join(""))) {
+        anagramas.push(palavra.join(""));
+        console.log(palavra.join(""));
+      }
     }
+  }
 });
